6a29d136f06ab228995a5a4f69fa61f3b5c04631
ZenosSeemsOk <150531078+ZenosSeemsOk@users.noreply.github.com>
2025-01-26 20:56:10 +0545

    Asteroids Resolved

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -16,36 +16,54 @@ public class SceneManagement : MonoBehaviour
             Destroy(Instance);
         }
         DontDestroyOnLoad(Instance);
-    }
 
-    private void Start()
-    {
         if (totalUnlockedLevel == 1)
         {
-            islands[0].islandNumberValue = 3;
+            islands[0].islandNumberValue = 4;
             islands[1].islandNumberValue = 3;
             islands[2].islandNumberValue = 2;
-            islands[3].islandNumberValue = 1; 
+            islands[3].islandNumberValue = 1;
 
             islands[4].islandNumberValue = 7;
             islands[5].islandNumberValue = 9;
             islands[6].islandNumberValue = 5;
-            islands[7].islandNumberValue = 8; 
-                
+            islands[7].islandNumberValue = 8;
+
             Debug.Log(islands[0].islandNumberValue + islands[1].islandNumberValue);
         }
         else if (totalUnlockedLevel == 2)
         {
+            islands[0].islandNumberValue = 8;
+            islands[1].islandNumberValue = 7;
+            islands[2].islandNumberValue = 6;
+            islands[3].islandNumberValue = 5;
 
+            islands[4].islandNumberValue = 2;
+            islands[5].islandNumberValue = 4;
+            islands[6].islandNumberValue = 5;
+            islands[7].islandNumberValue = 8;
         }
         else if (totalUnlockedLevel == 3)
         {
+            islands[0].islandNumberValue = 4;
+            islands[1].islandNumberValue = 3;
+            islands[2].islandNumberValue = 2;
+            islands[3].islandNumberValue = 1;
 
+            islands[4].islandNumberValue = 7;
+            islands[5].islandNumberValue = 9;
+            islands[6].islandNumberValue = 5;
+            islands[7].islandNumberValue = 8;
         }
         else
         {
             Debug.Log("Level index exceeds boundary");
         }
+    }
+
+    private void Start()
+    {
+
     }
 
     private void ChangeSprite()
--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 public class SpriteChanger : MonoBehaviour
 {
     public int level;
+    public int value;
     public Sprite[] sprites;
     private IslandSelection island;
     private SceneManagement sceneManager;
-
     private void Start()
     {
+        island = GetComponent<IslandSelection>();
         sceneManager = SceneManagement.Instance;
         level = sceneManager.totalUnlockedLevel;
-        island = GetComponent<IslandSelection>();
-        if (level == 1)
-        {
             switch (island.islandNumberValue)
             {
                 case 0:
@@ -37,13 +35,35 @@ public class SpriteChanger : MonoBehaviour
                     island.spriteRenderer.sprite = sprites[4];
                     Debug.Log($"Sprite changed for {island.islandID}");
                     break;
-                default:
-                    island.spriteRenderer.sprite = sprites[1];
+                case 5:
+                    island.spriteRenderer.sprite = sprites[5];
                     Debug.Log($"Sprite changed for {island.islandID}");
                     break;
-                
-              
+                case 6:
+                    island.spriteRenderer.sprite = sprites[6];
+                    Debug.Log($"Sprite changed for {island.islandID}");
+                    break;
+                case 7:
+                    island.spriteRenderer.sprite = sprites[7];
+                    Debug.Log($"Sprite changed for {island.islandID}");
+                    break;
+                case 8:
+                    island.spriteRenderer.sprite = sprites[8];
+                    Debug.Log($"Sprite changed for {island.islandID}");
+                    break;
+                case 9:
+                    island.spriteRenderer.sprite = sprites[9];
+                    Debug.Log($"Sprite changed for {island.islandID}");
+                    break;
+                default:
+                    island.spriteRenderer.sprite = sprites[4];
+                    Debug.Log($"Sprite changed for {island.islandID}");
+                    break;
             }
-        }
+    }
+
+    private void Update()
+    {
+        value = island.islandNumberValue;
     }
 }
